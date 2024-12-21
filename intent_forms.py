107809ea036d9c80cc17697f8926da183e7d6dfa
Jake Bailey <asaxplayinghorse@gmail.com>
2024-12-21 14:34:09 -0800

    Update intent forms script to properly handle non-responses.

--- intent_forms.py
+++ intent_forms.py
@@ -15,27 +15,53 @@ selectQuery = """
     SELECT * FROM mledb.player AS mp
     WHERE mp.mleid='{}'
 """
-dryRun = False
+
+fullPlayerTable = """
+    SELECT * FROM mledb.player AS mp
+"""
+
+dryRun = True
+players = {}
+
 with engine.connect() as conn:
-    with open('intent_forms.csv') as csvfile:
+    # First, read the current playerbase from the DB
+    fullTable = conn.execute(text(fullPlayerTable))
+    for row in fullTable:
+        did, name, mleid = row[14], row[6], row[5]
+        # Default to FP, since if they didn't respond that's where they're headed
+        players[mleid] = (did, name, 'FP')
+        
+    # Read the responses from the input CSV file
+    with open('inputs/mlebb_retentions.csv') as csvfile:
         reader = csv.reader(csvfile)
         for i,row in enumerate(reader):
             if i == 0:
                 continue
 
-            # This is the format of the CSV from DK
-            name = row[0]
-            mleid = row[1]
-            team = row[2]
-            
-            # Now let's put it into the DB:
-            if dryRun:
-                print(f'Updating player {name} ({mleid}) to team {team}')
-                queryStr = selectQuery.format(mleid)
-                res = conn.execute(text(queryStr))
-                print([row[5] for row in res])
+            # This is the format of the CSV from MLEBB
+            # discord id,name,mleid,retention,team,league
+            did, name, mleid, resp, team, _ = row
+
+            # Responses are one of (Retain, FA, Former Player)
+            if resp == 'Retention Eligible':
+                new_team = team
+            elif resp == 'Free Agent':
+                new_team = 'FA'
             else:
-                queryStr = basicUpdateQuery.format(team, mleid)
-                print(queryStr)
-                conn.execute(text(queryStr))
-                conn.commit()
+                new_team = 'FP'
+            
+            # Update the players dictionary with the new response
+            players[mleid] = (did, name, new_team)
+            
+    for mleid, (did, name, new_team) in players.items():
+        # Now let's put it into the DB:
+        if dryRun:
+            print(f'Updating player {name} ({mleid}) to team {new_team}')
+            queryStr = selectQuery.format(mleid)
+            res = conn.execute(text(queryStr))
+            print([row[5] for row in res])
+        else:
+            queryStr = basicUpdateQuery.format(new_team, mleid)
+            print(queryStr)
+            conn.execute(text(queryStr))
+            conn.commit()
